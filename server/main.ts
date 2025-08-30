9d22e6249cc54d8a1088dce84eab013db0285270
wuxxy <youwalkedoff@gmail.com>
2025-08-30 16:44:46 -0400

    changes

--- server/main.ts
+++ server/main.ts
@@ -1,38 +1,58 @@
-import { Application, Router } from "@oak/oak";
+import Fastify from "fastify";
+import websocket from "@fastify/websocket";
 import { PrismaClient } from "@prisma/client";
+import axios from "axios";
+import "dotenv"
+import qs from "qs";
 const prisma = new PrismaClient();
+const fastify = Fastify();
+await fastify.register(websocket);
 
-const router = new Router();
+fastify.get("/", async () => "Hello world");
 
-router.get("/", (ctx) => {
-  ctx.response.body = "Hello world";
+fastify.get("/redirect", async (request, reply) => {
+    try{
+        const { code } = request.query as { code?: string };
+        if (code == "" || code == undefined){
+            console.log("no code provided");
+            return reply.code(400).send();
+        }
+        const tokens = await getToken(code);
+        const userData = await axios.get("https://discord.com/api/v10/users/@me", {
+            headers: {
+                Authorization: `Bearer ${tokens.access_token}`,
+            },
+        });
+        const user = userData.data;
+        console.log(user)
+        return reply.code(200).send();
+    }catch (error) {
+        console.log(error)
+    }
 });
-router.get("/redirect", (ctx )=>{
-  const url = ctx.request.url;
-  const params = url.searchParams;
-
-  const code = params.get("code")
-  if (!code){
-    return ctx.throw(400)
-  }
-
-})
-router.get("/ws", (ctx) => {
-  if (!ctx.isUpgradable) {
-    ctx.throw(501);
-  }
-  const ws = ctx.upgrade();
-
-  ws.onopen = () => console.log("socket opened");
-  ws.onmessage = (e) => {
-    console.log("recv:", e.data);
-    ws.send("echo: " + e.data);
-  };
-  ws.onclose = () => console.log("socket closed");
-  ws.onerror = (err) => console.error("socket error:", err);
+async function getToken(code: string) {
+    const response = await axios.post(
+        "https://discord.com/api/v10/oauth2/token",
+        qs.stringify({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: "authorization_code",
+            code,
+            redirect_uri: process.env.REDIRECT_URI, // must match what you registered
+        }),
+        {
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+        }
+    );
+    return response.data;
+}
+fastify.get("/ws", { websocket: true }, (connection) => {
+    connection.socket.on("message", (msg) => {
+        connection.socket.send("echo: " + msg.toString());
+    });
 });
-const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
 
-app.listen();
+const port = Number(process.env.PORT ?? "6969");
+await fastify.listen({ port, host: "0.0.0.0" }).then(() => {console.log(`Listening on http://localhost:${port}`); });
